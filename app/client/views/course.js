516e8db380ad38bd13fbc4a4352ceebfb6017801
David Sykora <dsykora@gmail.com>
2015-10-25 07:31:19 -0700

    Basic lesson walk through

--- app/client/views/course.js
+++ app/client/views/course.js
@@ -1,3 +1,12 @@
+var lessonSteps = [ 'intro', 'login', 'dropUser', 'dropTable'];
+
+Template.course.onCreated(function(){
+	if(!_.isString(FlowRouter.getQueryParam('lessonStep'))){
+		FlowRouter.setQueryParams({lessonStep: lessonSteps[0]});
+	}
+});
+
+
 Template.course.helpers({
 	course: function(){
 		var course =  _.find(COURSES, function(course){
@@ -5,5 +14,41 @@ Template.course.helpers({
 		});
 		console.log('course: ', course);
 		return course;
+	},
+	onFirstStep: function(){
+		var step = FlowRouter.getQueryParam('lessonStep');
+		return _.indexOf(lessonSteps, step) === 0;
+	},
+	onLastStep: function(){
+		var step = FlowRouter.getQueryParam('lessonStep');
+		return _.indexOf(lessonSteps, step) === (lessonSteps.length -1);
 	}
 });
+
+
+Template.course.events = {
+	'click button[data-action="next"]': function(event, template){
+		event.preventDefault();
+		var message = this;
+		var step = FlowRouter.getQueryParam('lessonStep');
+		var stepIndex = _.indexOf(lessonSteps, step);
+		if(lessonSteps.length > stepIndex+1){
+			FlowRouter.setQueryParams({lessonStep: lessonSteps[stepIndex+1]});
+		}else{
+			FlowRouter.go('/');
+			FlowRouter.setQueryParams({lessonStep: null});
+			//Meteor.call('resetMessageTimer', message._id, moment().valueOf());
+		}
+	},
+	'click button[data-action="prev"]': function(event, template){
+		event.preventDefault();
+		var step = FlowRouter.getQueryParam('lessonStep');
+		var stepIndex = _.indexOf(lessonSteps, step);
+		if(stepIndex > 0){
+			FlowRouter.setQueryParams({lessonStep: lessonSteps[stepIndex-1]});
+		}else{
+			FlowRouter.go('/');
+			FlowRouter.setQueryParams({lessonStep: null});
+		}
+	}
+};
